0e21bbd9eac20f92cc09f6c9bb4c3008d30a4b12
Dius21 <virajdhara21@gmail.com>
2023-12-04 07:02:56 +0530

    printing a message and exploring functions

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Learning-C
  my first repo of basic C programs
+
+
+hey there!! hope you're having a fabulous day :)
--- c program1.c
+++ c program1.c
@@ -1,1 +1,17 @@
-#include stdio
+#include <stdio.h>
+#include <string.h>
+
+void not_main()
+{
+    char usr_stmt[100]="somewhere, somehow, your statement didn't reach till here";
+
+    //input statement from user
+    //scaf("%s", usr_stmt);
+
+    //output statement
+    printf("Hello world!!\n%s", usr_stmt);
+}
+
+void main(){
+    not_main();
+}
