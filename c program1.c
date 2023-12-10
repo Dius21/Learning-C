b5da345726731ce1073ac7657dd2e44a8ad4b20a
Dius21 <virajdhara21@gmail.com>
2023-12-10 12:25:16 +0530

    figured out scanf != scaf lol but now a different error bugging me

--- c program1.c
+++ c program1.c
@@ -1,17 +1,38 @@
 #include <stdio.h>
 #include <string.h>
 
-void not_main()
+int main()
 {
-    char usr_stmt[100]="somewhere, somehow, your statement didn't reach till here";
+    //functions menu
+    int choice = 0;
+    int x;
+    printf("Which function do you wanna execute?:-\n");
+    printf("1. single_line_input()\nType the corresponding number:");
+    x= scanf("%d", &choice);
+    switch (choice) 
+    {
+        case 1:
+            single_line_input();
+            break;
+        default:
+			printf("Invalid choice");
+    }
 
-    //input statement from user
-    //scaf("%s", usr_stmt);
-
-    //output statement
-    printf("Hello world!!\n%s", usr_stmt);
+    return 0;
 }
 
-void main(){
-    not_main();
+
+void single_line_input()
+{
+    int x;
+    char usr_stmt[100] = "somewhere, somehow, your statement didn't reach till here";
+     
+    //prompt for statment
+    printf("Enter your statement: ");
+
+    //input statement from user
+    x= scanf_s("%s", usr_stmt);
+    
+    //output statement
+    printf("Hello world!!\n%s\nYour statement was %i charachters long", usr_stmt, (int)strlen(usr_stmt));
 }
