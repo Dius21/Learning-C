b19a9464e43157a72603c34b596e8b5ae4551a28
Dius21 <virajdhara21@gmail.com>
2023-12-11 21:08:26 +0530

    my_app_1.c stable

--- c program1.c
+++ c program1.c
@@ -1,38 +1,38 @@
 #include <stdio.h>
 #include <string.h>
 
-int main()
-{
-    //functions menu
-    int choice = 0;
-    int x;
-    printf("Which function do you wanna execute?:-\n");
-    printf("1. single_line_input()\nType the corresponding number:");
-    x= scanf("%d", &choice);
-    switch (choice) 
-    {
-        case 1:
-            single_line_input();
-            break;
-        default:
-			printf("Invalid choice");
-    }
-
-    return 0;
-}
-
 
 void single_line_input()
 {
-    int x;
     char usr_stmt[100] = "somewhere, somehow, your statement didn't reach till here";
      
     //prompt for statment
     printf("Enter your statement: ");
 
     //input statement from user
-    x= scanf_s("%s", usr_stmt);
+    scanf("%s", usr_stmt); 
     
     //output statement
     printf("Hello world!!\n%s\nYour statement was %i charachters long", usr_stmt, (int)strlen(usr_stmt));
+}
+
+
+int main()
+{
+    //functions menu
+    int choice = 0;
+    
+    printf("Which function do you wanna execute?:-\n");
+    printf("1. single_line_input()\nType the corresponding number:");
+    scanf("%d", &choice);
+    switch (choice)
+    {
+    case 1:
+        single_line_input();
+        break;
+    default:
+        printf("Invalid choice");
+    }
+
+    return 0;
 }
